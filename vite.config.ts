8fb4980796bcf2db0da0d31f76bd59ac16d6e256
chithanh1710 <chithanh171004@gmail.com>
2024-11-20 12:27:01 +0700

    update

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,13 @@
-import { defineConfig } from "vite";
+import path from "path";
 import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: "dist", // Thư mục xuất bản (mặc định là 'dist')
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
   },
   server: {
     open: true,
